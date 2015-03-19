2be00240d439e471042f75940f6a682565ea7ae3
atulsbiet <atulsingh.biet@gmail.com>
2015-03-19 10:15:18 +0530

    Added project

---
+++ SpecFlowAndPageSelenium/Framework/Browser.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+
+namespace Framework
+{
+    public static class Browser
+    {
+        public static readonly IWebDriver webDriver = new ChromeDriver();
+
+        public static IWebDriver Driver
+        {
+            get
+            {
+                return webDriver;
+            }
+        }
+
+        public static void GoTo(string url)
+        {
+            webDriver.Url = url;
+        }
+
+        public static void Close()
+        {
+            webDriver.Close();
+        }
+    }
+}
